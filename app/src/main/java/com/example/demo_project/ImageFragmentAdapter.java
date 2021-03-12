4391d7e2ef2eb22f491fe201c23cc2e4c52c2fb6
Kanhaiya Kumar <kanhaiyasisteccs2015@gmail.com>
2021-03-12 07:54:07 +0530

    NavigationView + ToolBar + BottomNavigationView + Fragments

--- app/src/main/java/com/example/demo_project/ImageFragmentAdapter.java
+++ app/src/main/java/com/example/demo_project/ImageFragmentAdapter.java
@@ -8,11 +8,12 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentStatePagerAdapter;
 
 public class ImageFragmentAdapter extends FragmentStatePagerAdapter {
-  String[] images=new String[]{
-          "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwebmeup.com%2Fblog%2Fstock-images-to-avoid.html&psig=AOvVaw36WP1eepjc75LpoNmSemvI&ust=1615465628821000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCIju-Mncpe8CFQAAAAAdAAAAABAI",
-          "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F644437027909509488%2F&psig=AOvVaw36WP1eepjc75LpoNmSemvI&ust=1615465628821000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCIju-Mncpe8CFQAAAAAdAAAAABAO",
-          "https://www.google.com/url?sa=i&url=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fhorizon&psig=AOvVaw36WP1eepjc75LpoNmSemvI&ust=1615465628821000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCIju-Mncpe8CFQAAAAAdAAAAABAT"
-  };
+    String[] images = new String[]{
+            "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwebmeup.com%2Fblog%2Fstock-images-to-avoid.html&psig=AOvVaw36WP1eepjc75LpoNmSemvI&ust=1615465628821000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCIju-Mncpe8CFQAAAAAdAAAAABAI",
+            "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F644437027909509488%2F&psig=AOvVaw36WP1eepjc75LpoNmSemvI&ust=1615465628821000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCIju-Mncpe8CFQAAAAAdAAAAABAO",
+            "https://www.google.com/url?sa=i&url=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fhorizon&psig=AOvVaw36WP1eepjc75LpoNmSemvI&ust=1615465628821000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCIju-Mncpe8CFQAAAAAdAAAAABAT"
+    };
+
     public ImageFragmentAdapter(@NonNull FragmentManager fm, int behavior) {
         super(fm, behavior);
     }
@@ -20,9 +21,9 @@ public class ImageFragmentAdapter extends FragmentStatePagerAdapter {
     @NonNull
     @Override
     public Fragment getItem(int position) {
-        HomeFragment homeFragment=new HomeFragment();
-        Bundle bundle=new Bundle();
-        bundle.putString("source",images[position]);
+        IndexHomeFragment homeFragment = new IndexHomeFragment();
+        Bundle bundle = new Bundle();
+        bundle.putString("source", images[position]);
         homeFragment.setArguments(bundle);
         return homeFragment;
     }
--- app/src/main/java/com/example/demo_project/MainActivity.java
+++ app/src/main/java/com/example/demo_project/MainActivity.java
@@ -1,42 +1,82 @@
 package com.example.demo_project;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.Toast;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
-import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     private final BottomNavigationView.OnNavigationItemSelectedListener navListener = item -> {
         Fragment fragment = null;
         if (item.getItemId() == R.id.hom) {
-            fragment = new HomeFragment();
+            fragment = new IndexHomeFragment();
         } else if (item.getItemId() == R.id.settings) {
             fragment = new SettingsFragment();
         } else if (item.getItemId() == R.id.photo) {
             fragment = new TaskManger();
         }
+        assert fragment != null;
         getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, fragment).commit();
         return true;
     };
+    private DrawerLayout drawerLayout;
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.extra_menu,menu);
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ViewPager viewPager = findViewById(R.id.pager);
-        ImageFragmentAdapter imageFragmentAdapter = new ImageFragmentAdapter(getSupportFragmentManager(), 1);
-        viewPager.setAdapter(imageFragmentAdapter);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navmenu);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(item -> {
+            Fragment fragment = null;
+            if (item.getItemId() == R.id.hom) {
+                fragment = new IndexHomeFragment();
+                drawerLayout.closeDrawer(GravityCompat.START);
+            } else if (item.getItemId() == R.id.settings) {
+                fragment = new SettingsFragment();
+                drawerLayout.closeDrawer(GravityCompat.START);
+            } else if (item.getItemId() == R.id.photo) {
+                fragment = new TaskManger();
+                drawerLayout.closeDrawer(GravityCompat.START);
+            }
+            assert fragment != null;
+            getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, fragment).commit();
+            return true;
+        });
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_view);
 
         bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
 
-        getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, new HomeFragment()).commit();
+        getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, new IndexHomeFragment()).commit();
     }
-
 }
